0ff98a2bd4ac9b81180aea10519d83bea16ed16a
Arno Klein <arno@Mac.lan>
2025-10-08 18:24:19 -0400

    Edit docstring

--- keyboards/layouts_filter_patterns.py
+++ keyboards/layouts_filter_patterns.py
@@ -47,26 +47,24 @@ Usage:
 
     # Remove rows/layouts that have any of the top eight keys empty.
     python layouts_filter_patterns.py \
-        --input output/layouts_consolidate_moo_solutions.csv \
-        --output output/layouts_filter_empty_spaces.csv --report \
+        --input ../output/layouts_consolidate_moo_solutions.csv \
+        --output ../output/layouts_filter_empty_spaces.csv --report \
         --exclude "^.{2}[ ],^.{7}[ ],^.{11}[ ],^.{12}[ ],^.{13}[ ],^.{16}[ ],^.{17}[ ],^.{18}[ ]"
 
     
     # Keyboard layout optimization study command:
     # Don't permit common bigrams to be stacked vertically or to require hurdles.
-    # Up to 25% (cumulative fraction 0.249612493 at "or")
+    #   Up to 25% (cumulative fraction 0.249612493 at "or")
     BIGRAMS25="th,he,in,er,an,re,on,at,en,nd,ti,es,or"
-    # From 25% to 50% (cumulative fraction 0.497485843 at "ea")
+    #   25% to 50% (cumulative fraction 0.497485843 at "ea")
     BIGRAMS50="te,of,ed,is,it,al,ar,st,to,nt,ng,se,ha,as,ou,io,le,ve,co,me,de,hi,ri,ro,ic,ne,ea"
-    # From 50% to 75% (cumulative fraction 0.727855962 at "na")
+    #   50% to 75% (cumulative fraction 0.727855962 at "na")
     BIGRAMS75="ra,ce,li,ch,ll,be,ma,si,om,ur,ca,el,ta,la,ns,di,fo,ho,pe,ec,pr,no,ct,us,ac,ot,il,tr,ly,nc,et,ut,ss,so,rs,un,lo,wa,ge,ie,wh,ee,wi,em,ad,ol,rt,po,we,na"
-    # From 75% to 90% (cumulative fraction 0.921126837 at "au")
-    BIGRAMS90="ul,ni,ts,mo,ow,pa,im,mi,ai,sh,ir,su,id,os,iv,ia,am,fi,ci,vi,pl,ig,tu,ev,ld,ry,mp,fe,bl,ab,gh,ty,op,wo,sa,ay,ex,ke,fr,oo,av,ag,if,ap,gr,od,bo,sp,rd,do,uc,bu,ei,ov,by,rm,ep,tt,oc,fa,ef,cu,rn,sc,gi,da,yo,cr,cl,du,ga,qu,ue,ff,ba,ey,ls,va,um,pp,ua,up,lu,go,ht,ru,ug,ds,lt,pi,rc,rr,eg,au"
-    BIGRAMS="$BIGRAMS25,$BIGRAMS50" #,$BIGRAMS75" #,$BIGRAMS90"
-    HURDLE_BIGRAMS="$BIGRAMS25,$BIGRAMS50,$BIGRAMS75"
+    BIGRAMS="$BIGRAMS25,$BIGRAMS50"
+    HURDLE_BIGRAMS="$BIGRAMS25,$BIGRAMS50"
     poetry run python3 layouts_filter_patterns.py \
-        --input output/layouts_consolidate_moo_solutions.csv \
-        --output output/layouts_filter_patterns.csv --report \
+        --input ../output/layouts_consolidate_moo_solutions.csv \
+        --output ../output/layouts_filter_patterns.csv --report \
         --exclude-vertical-bigrams "$BIGRAMS" \
         --exclude-hurdles "$HURDLE_BIGRAMS"
 
